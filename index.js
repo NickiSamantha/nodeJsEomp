ad644fd97c667e575ebcd5e44ea14d6e9763faf3
mahmud-isaacs <mahmud.isaacs03@gmail.com>
2024-08-20 15:17:06 +0200

    edit

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import usersRouter from './routes/users.js'
 
 let port = process.env.PORT || 5001
 const app = express()
+app.use(express.static('public'))
 app.use(express.json())
 
 
 
-app.use(express.static('public'))
 app.use('/products', productsRouter)
 app.use('/users', usersRouter)
 
