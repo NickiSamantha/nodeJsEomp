8e238e3a37e81c57ea809d54cc784f477333865b
mahmud-isaacs <mahmud.isaacs03@gmail.com>
2024-08-21 10:29:41 +0200

    added try and catch

--- controller/productsController.js
+++ controller/productsController.js
@@ -9,27 +9,43 @@ const fetchProduct = async(req,res)=>{
     res.json(await getProductDb(req.params.id))
 }
 
-const insertProduct = async(req,res)=>{
-    let {prodName, quantity, amount, category, prodUrl} = req.body
-    await insertProductDb(prodName, quantity, amount, category, prodUrl)
-    res.send('Product was inserted successfully')
-}
+const insertProduct = async (req, res) => {
+    try {
+        let { prodName, quantity, amount, category, prodUrl } = req.body;
+        await insertProductDb(prodName, quantity, amount, category, prodUrl);
 
-const deleteProduct = async(req,res)=>{
-    await deleteProductDb(req.params.id)
-    res.send('Product was deleted successfully')
-}
+        res.status().send('Product was added successfully');
+    } catch (error) {
+        res.status(400).send('An error occurred while adding the product');
+    }
+};
 
-const updateProduct = async(req,res) =>{
-    let {prodName, quantity, amount, category, prodUrl} = req.body
-    let product = await getProductDb(req.params.id)
-    prodName?prodName=prodName:prodName = product.prodName
-    quantity?quantity=quantity:quantity = product.quantity
-    amount?amount=amount:amount = product.amount
-    category?category=category:category = product.category
-    prodUrl?prodUrl=prodUrl:prodUrl = product.prodUrl
-    await updateProductDb(prodName, quantity, amount, category, prodUrl, req.params.id)
-    res.send('Product was updated successfully')
-}
+const deleteProduct = async (req, res) => {
+    try {
+        await deleteProductDb(req.params.id);
+        res.send('Product was deleted successfully');
+    } catch (error) {
+        res.status(400).send('An error occurred while trying to delete a product');
+    }
+};
+
+
+const updateProduct = async (req, res) => {
+    try {
+        let { prodName, quantity, amount, category, prodUrl } = req.body;
+        let product = await getProductDb(req.params.id);
+        prodName?prodName=prodName:prodName = product.prodName
+        quantity?quantity=quantity:quantity = product.quantity
+        amount?amount=amount:amount = product.amount
+        category?category=category:category = product.category
+        prodUrl?prodUrl=prodUrl:prodUrl = product.prodUrl
+        await updateProductDb(prodName, quantity, amount, category, prodUrl, req.params.id);
+        res.send('Product was updated successfully');
+
+    } catch (error) {
+        res.status(400).send('An error occurred while updating the product');
+    }
+};
 
 export{fetchProducts,fetchProduct,insertProduct,deleteProduct,updateProduct}
+
--- controller/usersController.js
+++ controller/usersController.js
@@ -9,47 +9,48 @@ const fetchUser = async(req,res)=>{
     res.json(await getUserDb(req.params.id))
 }
 
-const insertUser = async(req,res)=>{
-    let {firstName, lastName, userAge, Gender, userRole, userAdd, userPass, userProfile} = req.body
-    
-    hash(userPass, 10, async (err,hashedP)=>{
-        await insertUserDb(firstName, lastName, userAge, Gender, userRole, userAdd, hashedP, userProfile )
-        res.send('User was inserted successfully')
-    })
-}
+const insertUser = async (req, res) => {
+    try {
+        let { firstName, lastName, userAge, Gender, userRole, userAdd, userPass, userProfile } = req.body;
+
+        hash(userPass, 10, async (err, hashedP) => {
+            if (err) {
+                return res.status(400).send('Error with password');
+            }
+            try {
+                await insertUserDb(firstName, lastName, userAge, Gender, userRole, userAdd, hashedP, userProfile);
+                res.status(200).send('User was registered successfully');
+            } catch (dbError) {
+                res.status(400).send('Error while registering user');
+            }
+        });
+    } catch (error) {
+        res.status(500).send('An error occurred while processing your request');
+    }
+};
 
 const deleteUser = async(req,res)=>{
     await deleteUserDb(req.params.id)
-    res.send('User was deleted successfully')
+    res.status(200).send('User was deleted successfully')
 }
 
-const updateUser = async(req,res)=>{
-    let {firstName,lastName,userAge,Gender,userRole,userAdd,userPass,userProfile} = req.body
-    let users = await getUserDb(req.params.id)
-    firstName?firstName=firstName:firstName = users.firstName
-    lastName?lastName=lastName:lastName = users.lastName
-    userAge?userAge=userAge:userAge = users.userAge
-    Gender?Gender=Gender:Gender = users.Gender
-    userRole?userRole=userRole:userRole = users.userRole
-    userAdd?userAdd=userAdd:userAdd = users.userAdd
-    userPass?userPass=userPass:userPass = users.userPass
-    userProfile?userProfile=userProfile:userProfile = users.userProfile
-    await updateUserDb(firstName,lastName,userAge,Gender,userRole,userAdd,userPass,userProfile,req.params.id)
-
-    // firstName ? firstName : firstName  = users.firstName
-    // lastName ? lastName :  lastName  = users.lastName
-    // userAge ? userAge :  userAge  = users.userAge
-    // Gender ? Gender :  Gender  = users.Gender
-    // userRole ? userRole :  userRole  = users.userRole
-    // userAdd ? userAdd :  userAdd = users.userAdd
-    // userPass ? userPass : userPass = users.userPass
-    // userProfile ? userProfile :  userProfile = users.userProfile
-
-    // await updateUserDb(firstName,lastName,userAge,Gender,userRole,userAdd,userPass,userProfile,+req.params.id)
-
-
-
-    res.send('User was updated successfully')
-}
+const updateUser = async (req, res) => {
+    try {
+        let { firstName, lastName, userAge, Gender, userRole, userAdd, userPass, userProfile } = req.body;
+        let users = await getUserDb(req.params.id);
+        firstName?firstName=firstName:firstName = users.firstName
+        lastName?lastName=lastName:lastName = users.lastName
+        userAge?userAge=userAge:userAge = users.userAge
+        Gender?Gender=Gender:Gender = users.Gender
+        userRole?userRole=userRole:userRole = users.userRole
+        userAdd?userAdd=userAdd:userAdd = users.userAdd
+        userPass?userPass=userPass:userPass = users.userPass
+        userProfile?userProfile=userProfile:userProfile = users.userProfile
+        await updateUserDb(firstName, lastName, userAge, Gender, userRole, userAdd, userPass, userProfile, req.params.id);
+        res.status(200).send('User was updated successfully');
+    } catch (error) {
+        res.status(400).send('An error occurred while updating the user');
+    }
+};
 
 export{fetchUsers,fetchUser,insertUser,deleteUser,updateUser}
