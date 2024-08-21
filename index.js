20375fb85e2481efc4369f8e5b6bb499d6e68e67
mahmud-isaacs <mahmud.isaacs03@gmail.com>
2024-08-21 12:11:21 +0200

    edit

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ let port = process.env.PORT || 5001
 const app = express()
 app.use(express.static('public'))
 app.use(express.json())
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Request-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization");
+  next();
+});
 
 
 app.use('/products', productsRouter)
