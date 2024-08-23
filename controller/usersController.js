2ddd5e93a873909c0cde56e8ef5278ad099f7770
mahmud-isaacs <mahmud.isaacs03@gmail.com>
2024-08-23 11:17:46 +0200

    fixed update

--- controller/usersController.js
+++ controller/usersController.js
@@ -38,6 +38,7 @@ const updateUser = async (req, res) => {
     try {
         let { firstName, lastName, userAge, Gender, userRole, userAdd, userPass, userProfile } = req.body;
         let users = await getUserDb(req.params.id);
+
         firstName?firstName=firstName:firstName = users.firstName
         lastName?lastName=lastName:lastName = users.lastName
         userAge?userAge=userAge:userAge = users.userAge
@@ -46,8 +47,23 @@ const updateUser = async (req, res) => {
         userAdd?userAdd=userAdd:userAdd = users.userAdd
         userPass?userPass=userPass:userPass = users.userPass
         userProfile?userProfile=userProfile:userProfile = users.userProfile
-        await updateUserDb(firstName, lastName, userAge, Gender, userRole, userAdd, userPass, userProfile, req.params.id);
-        res.status(200).send('User was updated successfully');
+
+       
+        if (userPass) {
+            hash(userPass, 10, async (err, hashedP) => {
+                if (err) {
+                    return res.status(404).send('Error with password');
+                }
+                userPass = hashedP; 
+                await updateUserDb(firstName, lastName, userAge, Gender, userRole, userAdd, userPass, userProfile, req.params.id);
+                return res.status(200).send('User was updated successfully');
+            });
+        } else {
+        
+            userPass = users.userPass;
+            await updateUserDb(firstName, lastName, userAge, Gender, userRole, userAdd, userPass, userProfile, req.params.id);
+            res.status(200).send('User was updated successfully');
+        }
     } catch (error) {
         res.status(404).send('An error occurred while updating the user');
     }
